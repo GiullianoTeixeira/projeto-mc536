ac7b5249a9d1a6793b5f14307ba26ded619e7035
romcol <e.rcolombo2@gmail.com>
2024-10-29 22:09:51 -0300

    add more fields to dotenv

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import os
 from dotenv import load_dotenv
 import model
 
-
 load_dotenv()
 
 app = Flask(__name__, template_folder='templates')
@@ -14,11 +13,11 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # Configure MySQL connection
-app.config["SECRET_KEY"] = "pipocas"
-app.config['MYSQL_HOST'] = 'localhost'
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = 'aquadata'
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 def get_db():
     """Opens a new database connection if there is none yet for the
