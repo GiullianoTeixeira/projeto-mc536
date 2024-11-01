ffce2164926eec5b34b320a40e6b9993b03e6b8d
GustavoJun <gustavogujun@gmail.com>
2024-11-01 17:53:24 -0300

    first sketch of water body details page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_login import *
 import mysql.connector
 import os
 from dotenv import load_dotenv
-import model
+import model, useful_queries
 
 load_dotenv()
 
@@ -20,18 +20,20 @@ app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'db'):
-        g.db = mysql.connector.connect(
-            host=app.config['MYSQL_HOST'],
-            user=app.config['MYSQL_USER'],
-            password=app.config['MYSQL_PASSWORD'],
-            database=app.config['MYSQL_DB']
-        )
+    if not hasattr(g, 'db') or not g.db.is_connected():
+        try:
+            g.db = mysql.connector.connect(
+                host=app.config['MYSQL_HOST'],
+                user=app.config['MYSQL_USER'],
+                password=app.config['MYSQL_PASSWORD'],
+                database=app.config['MYSQL_DB']
+            )
+        except mysql.connector.Error as err:
+            print(f"Database connection error: {err}")
+            g.db = None
     
     return g.db
+
 
 @app.teardown_appcontext
 def close_db(error):
@@ -41,7 +43,10 @@ def close_db(error):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return model.get_user_by_id(get_db(), user_id)
+    db = get_db()
+    if not db or not db.is_connected():
+        print("DB connection in load_user() is not available")
+    return model.get_user_by_id(db, user_id)
 
 @app.route('/')
 def login():
@@ -98,6 +103,31 @@ def register():
     elif request.method == "GET":
         return render_template("register.html")
 
+@app.route('/waterbody/<int:waterbody_id>', methods=['GET'])
+def waterbody(waterbody_id):
+    
+    connection = get_db()
+    # Fetch waterbody details and related statistics
+    (
+    waterbody_data, 
+    count_complaints, 
+    count_reports, 
+    media_indice_biodiversidade, 
+    ph_stats, 
+    denuncias_por_severidade
+    ) = useful_queries.get_waterbodydata_by_id(connection, waterbody_id)
+
+    connection.close()
+
+    return render_template(
+        "waterbody.html",
+        waterbody=waterbody_data,
+        count_complaints=count_complaints,
+        count_reports=count_reports,
+        media_indice_biodiversidade=media_indice_biodiversidade,
+        ph_stats=ph_stats,
+        denuncias_por_severidade=denuncias_por_severidade
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
