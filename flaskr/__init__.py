9f0a70d5871ae05f5cc7adfd4bee15945a7c1b26
Cainhu19 <caioenzo92@outlook.com>
2024-11-03 16:12:31 -0300

    Fixed secret key

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,14 +1,14 @@
 import os
 from flask import *
+from dotenv import load_dotenv
 from . import db, auth, waterbody, search
 
+load_dotenv()
 def create_app(test_config=None):
+    
     # create and configure the app
     app = Flask(__name__, template_folder='templates', instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
     @app.route('/')
     def login():
